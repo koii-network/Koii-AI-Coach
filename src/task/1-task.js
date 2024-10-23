1a1436fbf34c831de6ce31aa9a5cdf0844c0412f
Herman Liang <herman@koii.network>
2024-10-23 15:55:40 -0300

    add checkDiskUsage and add try catch

--- src/task/1-task.js
+++ src/task/1-task.js
@@ -6,7 +6,7 @@ import os from 'os';
 import { uploadToIPFS } from './utils/uploadToIPFS.js';
 import path from 'path';
 import { getBasePath } from './utils/getBaseInfo.js';
-
+import { checkDiskUsage } from './utils/checkDiskUsage.js';
 export async function task(roundNumber) {
   // Run your task and store the proofs to be submitted for auditing
   // The submission of the proofs is done in the submission function
@@ -19,16 +19,21 @@ export async function task(roundNumber) {
   
     // Create a JSON object to store the value
     const value = {};
-    value.m = "llama3.2";
-    // Check RAM
-    const totalMemory = os.totalmem();
-    value.r = `${(totalMemory / 1024 / 1024 / 1024).toFixed(2)}`;
-    // Check OS
-    value.o = os.platform();
-    // Check Available Disk Space
-    const freeDiskSpace = os.freemem();
-    value.d = `${(freeDiskSpace / 1024 / 1024 / 1024).toFixed(2)}`;
-
+    try{
+      value.m = "llama3.2";
+      // Check RAM
+      const totalMemory = os.totalmem();
+      value.r = `${(totalMemory / 1024 / 1024 / 1024).toFixed(2)}`;
+      // Check OS
+      value.o = os.platform();
+      // Check Available Disk Space
+      const freeMemorySpace = os.freemem();
+      value.d = `${(freeMemorySpace / 1024 / 1024 / 1024).toFixed(2)}`;
+      const freeDiskSpace = await checkDiskUsage();
+      value.f = freeDiskSpace;
+    }catch(e){
+      console.log(e);
+    }
     // Check the model accessibility
     try {
       const response = await ollama.chat({
