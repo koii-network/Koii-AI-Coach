5e8def62347c0fc46038a57c8089f4f52e3ceea7
Herman Liang <herman@koii.network>
2024-10-23 20:13:33 -0300

    model updated

--- tests/ollamaTest.js
+++ tests/ollamaTest.js
@@ -1,7 +1,7 @@
 import ollama from 'ollama';
 async function main(){
     const result = await ollama.chat({
-        model: 'sb',
+        model: 'koiiLlama',
         messages: [{role: 'user', content: 'Hello, how are you?'}],
         options: {}
     });
