690f307e7928fe4be54c96db30e4f2cbcbbe0d9f
Herman Liang <herman@koii.network>
2024-11-08 14:39:41 -0400

    update audit logic

--- src/task/3-audit.js
+++ src/task/3-audit.js
@@ -1,44 +1,61 @@
 import { namespaceWrapper, TASK_ID } from "@_koii/namespace-wrapper";
 import { getAccessLink } from "./utils/getBaseInfo.js";
-
+import ollama from 'ollama';
 export async function audit(submission, roundNumber, submitterPublicKey) {
   /**
    * Audit a submission
    * This function should return true if the submission is correct, false otherwise
    */
-
   const submissionJSON = JSON.parse(submission);
-  const { m: model } = submissionJSON;
-
+  const { b: answer } = submissionJSON;
+  if (!answer || answer.length === 0 || answer.length > 5) {
+    return false;
+  }
   console.log(`AUDIT SUBMISSION FOR ROUND ${roundNumber}`); 
   // First: Check if model is accessible
   const IPAddressArray = await getAddressArray();
-
+  // const nodeAddress = undefined;
   const nodeAddress = IPAddressArray[submitterPublicKey];
+  if (!nodeAddress) {
+    return false;
+  }
   console.log("Validating", nodeAddress);
-  const query = `Hey! How are you?`;
+
   const accessLink = await getAccessLink(nodeAddress);
-  console.log("Access Link", accessLink);
-  if (!accessLink) {
-    return false;
+  const result = await validateModel(accessLink);
+  if (result === undefined) {
+    return;
   }
-  const response = await fetch(`${accessLink}/ask-query`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' }, 
-    body: JSON.stringify({ model: model, query: query }) 
-  });
+  return result;
+}
+
+async function validateModel(accessLink){
+  let question;
+  let result;
   try{
-    const data = await response.json(); 
-    if (data.reply) {
-      return true;
-    }
-    return false;
-  } catch (error) {
+    question = await ollama.chat(
+    { "model":"koiiLlama", "messages": [ {"role":"system", "content":"You are a helpful assistant."}, {"role":"user", "content":"Please generate a question"}], "options":{"num_predict":50, "temperature":1}});
+    console.log("Question", question.message.content);
+    result = await ollama.chat(
+      { "model":"koiiLlama", "messages": [ {"role":"system", "content":"You are a helpful assistant."}, {"role":"user", "content":question.message.content}], "options":{"num_predict":10, "temperature":0}});
+    console.log("Result", result.message.content);
+  } catch (error){
+    console.log("Error in generating question", error);
+    return;
+  }
+  try{
+    const response = await fetch(`${accessLink}/ask-query`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }, 
+      body: JSON.stringify({ "model":"koiiLlama", "messages": [ {"role":"system", "content":"You are a helpful assistant."}, {"role":"user", "content":question.message.content}], "options":{"num_predict":10, "temperature":0}}) 
+    });
+    const otherNodesResult = await response.json();
+    console.log(otherNodesResult.reply);
+    return compareString(result.message.content, otherNodesResult.reply);
+  }catch (error){
+    console.log("Error in getting access link", error);
     return false;
   }
-  // TODO: Check the views of the Tweets
-
-
 }
 
 async function getAddressArray() {
@@ -52,4 +69,8 @@ async function getAddressArray() {
   } catch (e) {
     console.log('ERROR GETTING TASK STATE', e);
   }
+}
+
+async function compareString(string1, string2){
+  return string1 === string2;
 }
