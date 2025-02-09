487a9530e832a8c393f6b0e903a8259a4e4e6d51
herman124410 <herman@koii.network>
2025-02-09 19:11:51 -0400

    submission updated to not submit when tasks link is not accessible

--- src/task/2-submission.js
+++ src/task/2-submission.js
@@ -7,6 +7,17 @@ export async function submission(roundNumber) {
    */
   try {
     console.log(`MAKE SUBMISSION FOR ROUND ${roundNumber}`);
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    const nodeKeypair = await namespaceWrapper.getSubmitterAccount();
+    const nodeAddress = nodeKeypair.publicKey.toBase58();
+    const tasksLink = await getTasksLink(nodeAddress);
+    if (!tasksLink){
+      return;
+    }
+    const accessibleValidationResult = await validateNetworkAccessible(tasksLink)
+    if (!accessibleValidationResult){
+      return;
+    }
     return await namespaceWrapper.storeGet("value");
   } catch (error) {
     console.error("MAKE SUBMISSION ERROR:", error);
