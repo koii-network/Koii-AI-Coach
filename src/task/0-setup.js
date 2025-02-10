b90b18283174a0fc7502966c94c5b66e3a6021d4
herman124410 <herman@koii.network>
2025-02-10 06:14:49 -0400

    save some time for mac os

--- src/task/0-setup.js
+++ src/task/0-setup.js
@@ -6,6 +6,10 @@ export async function setup() {
   // define any steps that must be executed before the task starts
   console.log("CUSTOM SETUP");
   console.log("Setup", new Date(), "TEST");
+  // Check if it is Mac
+  if (os.platform() === "darwin"){
+    return;
+  }
   if (os.platform() === "win32"){
     await initializeOllamaWindows();
   }else{
